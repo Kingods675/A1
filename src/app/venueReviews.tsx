e4b21865cff075a305438894e5e1f5270773670b
forsc1 <s4088927@student.rmit.edu.au>
2026-04-17 14:10:52 +1000

    Creating venue reviews component.

---
+++ src/app/venueReviews.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Box, Heading, Text, VStack } from '@chakra-ui/react';
+
+type Review = {
+  id: number;
+  venueId: number;
+  rating: number;
+  comment: string;
+};
+
+
+
