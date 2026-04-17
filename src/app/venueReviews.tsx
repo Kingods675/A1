90a6ea1ce5ae637b4dee96b81652d94051518425
forsc1 <s4088927@student.rmit.edu.au>
2026-04-17 14:26:04 +1000

    Implementing function to load reviews from localStorage

--- src/app/venueReviews.tsx
+++ src/app/venueReviews.tsx
@@ -9,5 +9,16 @@ type Review = {
   comment: string;
 };
 
+export default function VenueReviews({ venueId }: { venueId: number }) {
+  let reviews: Review[] = []; // empty list to hold reviews
 
 
+  if (typeof window !== 'undefined') { //to check if we're in the browser, fetching reviews from localStorage
+    //loads reviews from localStorage
+    const savedReviews = localStorage.getItem('vv_reviews');
+    if (savedReviews) {
+        reviews = JSON.parse(savedReviews);
+    }
+}
+
+  }
